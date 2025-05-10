00c7ca4628ae6989e54a21e971bfac5fc1c641c2
cyh7834 <cyh7834@gmail.com>
2025-05-10 20:30:06 +0900

    객체 타입의 호환성

--- section3/src/chapter3.ts
+++ section3/src/chapter3.ts
@@ -1,51 +1,75 @@
 /**
- * Unknown 타입. 최상위 슈퍼타입
+ * 객체 타입 간의 호환성
+ * -> 어떤 객체 타입을 다른 객체 타입으로 취급해도 괜찮은가?
  */
 
-function unKnownExam() {
-    let a: unknown = 1;
-    let b: unknown = "hello";
-    let c: unknown = true;
-    let d: unknown = null;
-    let e: unknown = undefined;
+type Animal = {
+    name: string;
+    color: string;
 }
+
+type Dog = {
+    name: string;
+    color: string;
+    breed: string;
+}
+
+let animal: Animal = {
+    name: "기린",
+    color: "yellow"
+};
+
+let dog: Dog = {
+    name: "돌돌이",
+    color: "brown",
+    breed: "진도"
+};
 
 /**
- * Never 타입. 모든 집합의 부분 집합. 최하위 타입. 공집합.
+ * 반대의 경우에는 오류가 발생
+ * Dog는 Animal 타입의 프로퍼티를 포함하며 추가로 프로퍼티를 갖음
+ * 프로퍼티가 적은 쪽이 슈퍼 타입이 됨
  */
 
-function neverExam() {
-    function neverFunc(): never {
-        while(true) {}
-    }
+animal = dog;
 
-    let num: number = neverFunc(); // 업 캐스팅
+type Book = {
+    name: string;
+    price: number;
 }
+
+type ProgrammingBook = {
+    name: string;
+    price: number;
+    skill: string;
+};
+
+let book: Book;
+let programmingBook: ProgrammingBook = {
+    name: "한 입 크기로 잘라먹는 리액트",
+    price: 33000,
+    skill: "reactjs"
+};
+
+book = programmingBook;
 
 /**
- * Void 타입
+ * 초과 프로퍼티 검사가 발생하여 오류 발생
  */
-
-function voidExam() {
-    function viodFunc(): void {
-        console.log("hi");
-
-        return undefined; // 업 캐스팅과 비슷한 상황
-    }
-
-    let voidVar: void = undefined;
+let book2: Book = {
+    name: "한 입 크기로 잘라먹는 리액트",
+    price: 33000,
+    //skill: "reactjs"
 }
+
+function func(book: Book) {}
 
 /**
- * Any 타입. 최상위 타입과 최하위 타입 모두 포함. never 제회
+ * 마찬가지로 오류가 발생하고, 사용하려면 선언해둔 변수를 전달해야 함
  */
-
-function anyExam() {
-    let unkKnownVar: unknown;
-    let anyVar: any;
-    let undefinedVar: undefined;
-
-    anyVar = unkKnownVar;
-
-    undefinedVar = anyVar;
-}
+func({
+    name: "한 입 크기로 잘라먹는 리액트",
+    price: 33000,
+    //skill: "reactjs"
+})
+func(programmingBook);
