5868d9554607b6f63d95b4bf32201ef425a246d0
cyh7834 <cyh7834@gmail.com>
2025-07-12 20:11:24 +0900

    infer

--- section9/src/chapter2.ts
+++ section9/src/chapter2.ts
@@ -1,29 +1,38 @@
 /**
- * infer
+ * infer 키워드
+ * - 조건부 타입 내에서 타입을 추론할 때 사용된다.
+ * - 주로 제네릭 타입을 분해하거나 특정 타입만 추출할 때 활용된다.
  */
 
-//조건부 타입 내에서 특정 타입만 추론
-
-type Func = () => string;
-
-type FuncB = () => number;
-
-type ReturnType<T> = T extends () => infer R ? R : never;
-
-type A = ReturnType<Func>;
-
-type B = ReturnType<FuncB>;
+// 예시 함수 타입 선언
+type Func = () => string;   // string을 반환하는 함수
+type FuncB = () => number;  // number를 반환하는 함수
 
 /**
- * 예제
+ * ReturnType<T>
+ * - 함수 타입 T가 () => R 형태일 때, 그 반환 타입 R을 추론하여 반환한다.
+ * - 그렇지 않으면 never를 반환한다.
  */
+type ReturnType<T> = T extends () => infer R ? R : never;
 
-// promise의 타입만 떼와서 사용
+type A = ReturnType<Func>;   // string
+type B = ReturnType<FuncB>;  // number
 
+// 위 결과:
+// - Func는 () => string이므로 infer R = string => ReturnType<Func> = string
+// - FuncB는 () => number이므로 infer R = number => ReturnType<FuncB> = number
+
+/**
+ * PromiseUnpack<T>
+ * - 제네릭 타입 T가 Promise<R> 형태일 때, 내부 값 R을 추론하여 반환한다.
+ * - 그렇지 않으면 never를 반환한다.
+ */
 type PromiseUnpack<T> = T extends Promise<infer R> ? R : never;
-// 1. T는 프로미스 타입이어야 한다.
-// 2. 프로미스 타입의 결과 값 타입을 반환해야 한다.
 
-type PromiseA = PromiseUnpack<Promise<number>>;
+// 사용 예시
+type PromiseA = PromiseUnpack<Promise<number>>; // number
+type PromiseB = PromiseUnpack<Promise<string>>; // string
 
-type PromiseB = PromiseUnpack<Promise<string>>;
+// 위 결과:
+// - Promise<number>는 infer R = number => PromiseUnpack = number
+// - Promise<string>은 infer R = string => PromiseUnpack = string
